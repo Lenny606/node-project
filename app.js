29e962d428a60c98697bcfaa0debdbecfeb17e1f
Lenny606 <92226943+Lenny606@users.noreply.github.com>
2024-03-29 09:29:06 +0100

    api

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send("Hello...")
+    res.status(200).json({
+        message: "hello from server side"
+    })
 })
+
+app.post('/', (req, res) => {
+    res.status(200).json({
+        message: "hello from post response"
+    })
+})
+
+
+
+
+
 const port = 3000;
 app.listen(port, ()=>{
     console.log('App running on port ' + port)
